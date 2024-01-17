3d0e058be1e3f7580ec08e55f5f3eea983663c2d
Wout Blomme <wout.blomme@student.howest.be>
2024-01-17 19:42:03 +0100

    commit

--- src/ai chat/config.ts
+++ src/ai chat/config.ts
@@ -5,9 +5,9 @@ export const Config: Configuration = {
     'https://storage.googleapis.com/assets-inworld-ai/models/innequin/v2/models/animations/emotions/',
   DEFAULT_ANIMATION: 'Neutral_Hello_Long_01',
   GENERATE_TOKEN_URL:
-    process.env.REACT_APP_GENERATE_TOKEN_URL || 'http://localhost:4000',
+    process.env.REACT_APP_GENERATE_TOKEN_URL || 'https://generate-token-krid.onrender.com/',
   GENERATE_TOKEN_URL1:
-    process.env.REACT_APP_GENERATE_TOKEN_URL1 || 'http://localhost:4000/red',
+    process.env.REACT_APP_GENERATE_TOKEN_URL1 || 'https://generate-token-krid.onrender.com/red',
   IMAGES_BODY_URI:
     'https://storage.googleapis.com/assets-inworld-ai/models/innequin/v2/textures/body/', // ./assets/textures/body/
   IMAGES_FACIAL_URI:
